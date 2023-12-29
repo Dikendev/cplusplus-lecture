b2b25cfc17eba11c56af9eb65ba12aa5a8a414d4
Dikendev <diken.dev@gmail.com>
2023-12-28 23:40:17 -0300

    add class basics

--- lecture.cpp
+++ lecture.cpp
@@ -42,14 +42,48 @@ public:
   }
 };
 
+class Course
+{
+  std::string m_name = "Course";
+  std::vector<Student> m_students;
+
+public:
+  Course() {}
+  Course(const std::string &name)
+      : m_name(name)
+  {
+  }
+
+  void addStudent(const Student &s)
+  {
+    m_students.push_back(s);
+  }
+
+  const std::vector<Student> &getStudent() const
+  {
+    return m_students;
+  }
+
+  void print() const
+  {
+    for (const auto &s : m_students)
+    {
+      s.print();
+    }
+  }
+};
+
 int main(int argc, char *argv[])
 {
 
   Student s1;
   Student s2("John", "Doe", 1, 3.14);
-  const Student s3("Jane", "Doe", 202212, 99.9);
-  s2.print();
-  s3.print();
+  Student s3("Jane", "Doe", 202212, 99.9);
+  Course cplusplus("C++");
+  cplusplus.addStudent(s2);
+  cplusplus.addStudent(s3);
+  cplusplus.addStudent(Student("Chris", "Doe", 2, 23.2));
+  cplusplus.print();
 
   return 0;
 }
